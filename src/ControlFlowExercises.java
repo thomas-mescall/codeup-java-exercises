58c84fdfea5555be2c46e716e38f4cbc60274336
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-27 09:19:48 -0500

    starting control flow

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,12 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] arrgs) {
+
+        int i = 0;
+        while (i < 15) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
