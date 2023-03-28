5a2573ff5f72fdd4973bf8ce4ccaa11c396db63f
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-28 15:55:57 -0500

    currently working on factorial

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     //public static void main(String[] args) {
         public static int addition(int x, int y){
@@ -8,11 +10,23 @@ public class MethodsExercises {
             return x - y;
         }
 
-        public static int multiplication(int x, int y){
-            return x * y;
+        public static int multiply(int x, int y){
+//            return multiplication(x, y);
+            int temp = 0;
+//            for(int i = 0; i < y; i++){
+//                temp += x;
+//            }
+//            return temp;
+            if(y > 0) {
+                temp = x + multiply(x, y - 1);
+            }
+            return temp;
         }
 
         public static int division(int x, int y){
+            if(y == 0) {
+                return 0;
+            }
             return x / y;
         }
 
@@ -20,13 +34,59 @@ public class MethodsExercises {
             return x % y;
         }
 
-        public static int call(){
-            return addition(5,5),
-                    subtraction(5,5),
-                    multiplication(5,5);
-                    division(5,5);
-                    modulus(5,5);
+        public static int getInteger(int min, int max) {
+
+
+            Scanner myScanner = new Scanner(System.in);
+            int userIntInput;
+            do {
+                System.out.println("Enter a number");
+                while(!myScanner.hasNextInt()){
+                    System.out.println("Your number is not valid");
+                    myScanner.next();
+                }
+                userIntInput = myScanner.nextInt();
+            }while(userIntInput < max && userIntInput > min);
+
+            return userIntInput;
         }
 
+        public static int factorial(){
+            Scanner myScanner = new Scanner(System.in);
+            int userinput1;
+            //String userinput2;
+            System.out.println("Enter an integer between 1 and 10");
+            userinput1 = myScanner.nextInt();
+            int factor = 1;
+            for(int i = 1; i <= userinput1; i++) {
+                factor *= i;
+            }
+//            System.out.printf("Your new number is: %d", userinput1);
+//            myScanner.nextLine();
+//            System.out.println("Would you like to continue? (y/n)");
+//            userinput2 = myScanner.nextLine();
+
+            return factor;
+        }
+
+    //function for testing
+    public static void main(String[] args) {
+
+
+        int numOne = 5;
+        int numTwo = 10;
+
+        System.out.println("Q3");
+        System.out.println(factorial());
+        System.out.println("Q2");
+        System.out.println(getInteger(numOne, numTwo));
+        System.out.println("Q1");
+        System.out.println(addition(numOne, numTwo));
+        System.out.println(subtraction(numOne, numTwo));
+        System.out.println(multiply(numOne, numTwo));
+        System.out.println(division(numTwo, numOne));
+        System.out.println(modulus(numOne, numTwo));
     }
-//}
+
+
+    }
