6e9b13c0c3b24761744e1952cbb2a2a45930750e
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-29 15:44:31 -0500

    almost done

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,46 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    private Scanner myScanner;
+
+    public Input(){
+        myScanner = new Scanner(System.in);
+        String userInput = myScanner.next();
+    }
+
+    public String getString(){
+        System.out.println("Enter a String");
+        return myScanner.next();
+    }
+
+    public boolean yesNo(){
+        System.out.println("Si or No");
+        String userInput = myScanner.next();
+        return userInput.equalsIgnoreCase("Si");
+    }
+
+    public int getInt(int min, int max) {
+        min = 2;
+        max = 9;
+        int num = getInt();
+        while(num < min && num > max) {
+            System.out.println("Pick a number between 2 - 9");
+        }
+        return num;
+    }
+
+    public int getInt(){
+        System.out.println("Enter an integer: ");
+        while(!myScanner.hasNextInt()) {
+            System.out.println("Please enter a valid integer: ");
+            myScanner.next();
+        }
+        return myScanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
