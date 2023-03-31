a2dfeadf0459990bbcf096a438d8a062bf377cf9
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-30 22:24:53 -0500

    almost done with movies, need to figure out switch statment/getting the categories from movie array

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,17 +1,34 @@
 package movies;
 import util.Input;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MoviesApplication {
     public static void main(String[] args) {
+        Movie[] movies = MoviesArray.findAll();
         Input input = new Input();
+        System.out.printf("Would you like to view movies by category or all movies?%n0: Quit%n1: By Category%n2: All Movies");
+           int userinput = input.getInt(0,2);
+           while(userinput != 0){
+               if(userinput == 1){
+                   for(Movie movie : movies){
+                       System.out.printf("%s - %s%n", movie.getName(), movie.getCategory());
+                   }
+               } else if(userinput == 2){
+                   System.out.printf("What category would you like to view?%n1: animated%n2: drama%n3: horror%n4: scifi");
+                   int userCategory = input.getInt(1,4);
+                   for(Movie movie : movies){
+                       if(movie.getCategory().equals(movie.getCategory())){
+                           System.out.printf("%s - %s%n",movie.getName(), movie.getCategory());
+                       }
+                   }
+               }
+           }
+    }
+    public int getCategories(){
+
         Scanner myScanner = new Scanner(System.in);
-        System.out.printf("Would you like to view movies by category or all movies?%n1: By Category%n2: All Movies");
-        MoviesArray[] animated = movies.MoviesArray();
-        MoviesArray[] drama;
-        MoviesArray[] horror;
-        MoviesArray[] scifi;
         int userInput = myScanner.nextInt();
         boolean stopper = false;
         while(!stopper) {
@@ -19,12 +36,16 @@ public class MoviesApplication {
                 System.out.printf("What category would you like to view?%n1: animated%n2: drama%n3: horror%n4: scifi");
                 switch (userInput){
                     case 1:
+                        //"%s-%s%n",Movie.getName(), Movie.getCategory("animated")
                         break;
                     case 2:
+
                         break;
                     case 3:
+
                         break;
                     case 4:
+
                         break;
                 }
                 stopper = true;
@@ -36,5 +57,6 @@ public class MoviesApplication {
                 userInput = myScanner.nextInt();
             }
         }
-    }
+        return userInput;
+    };
 }
