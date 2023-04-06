e0f7f47f123807eca7efe64cdc004d7aaab86bea
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-04-06 12:04:11 -0500

    lunch time

--- src/util/Input.java
+++ src/util/Input.java
@@ -7,52 +7,61 @@ public class Input {
 
     public Input(){
         myScanner = new Scanner(System.in);
-        String userInput = myScanner.next();
+        //String userInput = myScanner.next();
     }
 
     public String getString(){
-        System.out.println("Enter a String");
+        //System.out.println("Enter a String");
         return myScanner.next();
     }
 
     public boolean yesNo(){
-        System.out.println("Si or No");
+        System.out.println("Yes or No");
         String userInput = myScanner.next();
-        return userInput.equalsIgnoreCase("Si");
+        return userInput.equalsIgnoreCase("Yes");
     }
 
     public int getInt(){
-        System.out.printf("%nEnter an integer: 0 - 2%n");
-        while(!myScanner.hasNextInt()) {
-            System.out.println("Please enter a valid integer: ");
-            myScanner.next();
+        System.out.printf("%nEnter an integer: 0 - 5%n");
+        int count = 0;
+        while(true) {
+            if(myScanner.nextInt() < 0 && myScanner.nextInt() > 5){
+                return Integer.valueOf(getString());
+            } else{
+                count++;
+                if(count > 3){
+                    throw new RuntimeException("YOU HAVE EXCEED THE MAXIMUM ATTEMPTS. PLEASE LEAVE");
+                }
+                System.out.println("Please enter a valid integer: ");
+            }
         }
-        return myScanner.nextInt();
     }
 
     public int getInt(int min, int max) {
-        int num = -1;
+        int num = 0;
         while(num < min || num > max) {
             num = getInt();
-            System.out.println("Pick a number between 0 - 2");
+            System.out.println("Pick a number between" + min + " - " + max);
         }
         return num;
     }
 
     public double getDouble(){
-        System.out.println("Enter a Double: ");
-        while(!myScanner.hasNextDouble()) {
+        System.out.println("Enter a Double: 0 - 5");
+        int count = 0;
+        while(!myScanner.hasNextDouble() || count != 3) {
             System.out.println("Please enter a valid Double: ");
-            myScanner.next();
+            count++;
+            //myScanner.next();
         }
-        return myScanner.nextDouble();
+        return Double.valueOf(getString());
     }
 
     public double getDouble(double min, double max) {
         double num = 0;
         while(num < min || num > max) {
             num = getDouble();
-            System.out.println("Pick a number between 2 - 9");
+            System.out.println("Pick a number between" + min + " - " + max);
         }
         return num;
     }
